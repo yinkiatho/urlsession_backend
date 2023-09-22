2ba5617fcc42d773b4db42ac123ec37e4bcb40b5
yinkiatho <108441585+yinkiatho@users.noreply.github.com>
2023-09-22 17:53:14 +0800

    added lazy dev

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const upload = require('./helpers/multer_config').upload;
+const { spawn } = require('child_process');
+
+//run python script
+const {PythonShell} = require('python-shell');
+
 
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
     res.status(200).json({ success: 'Hello Server' });
+});
+
+app.post("/lazy-developer", (req, res) => {
+    //run lazy-developer.py with json input from req.body
+    //res.status(200).json({ success: 'Hello Server' });
+    requestData = req.body
+    const pythonProcess = spawn('python', ['lazy_dev.py', JSON.stringify(requestData)]);
+
+    // Handle data from the Python script
+    pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python Output: ${data}`);
+    res.json({ message: data.toString() });
+    });
+
+  // Handle errors (if any)
+    pythonProcess.stderr.on('data', (data) => {
+    console.error(`Error: ${data}`);
+    res.status(500).json({ error: 'An error occurred' });
+  });
+    
 });
 
 app.post('/upload', upload.array('imageUploads', 10), (req, res) => {
---
+++ src/lazy_dev.py
@@ -0,0 +1,139 @@
+import json
+from typing import Dict, List
+import re
+
+classes = [
+    {
+        "Order": {
+            "orderId": "String",
+            "version": "Long",
+            "orderType": "OrderType",
+            "orderSide": "OrderSide",
+            "status": "Status",
+            "allocations": "List<Allocation>"
+        }
+    },
+    {
+        "OrderType": [
+            "MarketOrderType",
+            "LimitOrderType"
+        ]
+    },
+    {
+        "MarketOrderType": ""
+    },
+    {
+        "LimitOrderType": {
+            "price": "Double"
+        }
+    },
+    {
+        "OrderSide": [
+            "Buy",
+            "Sell"
+        ]
+    },
+    {
+        "Status": [
+            "New",
+            "Verifying",
+            "Pending",
+            "Working",
+            "PartiallyFilled",
+            "Filled",
+            "Cancelled"
+        ]
+    },
+    {
+        "Allocation": [
+            "LongAllocation",
+            "EmptyAllocation"
+        ]
+    },
+    {
+        "LongAllocation": {
+            "clientName": "String"
+        }
+    },
+    {
+        "EmptyAllocation": ""
+    }]
+statements = [
+    "Order.",
+    "Order.order",
+    "Order.allocations.",
+    "Status.P",
+    "MarketOrderType."
+]
+
+
+def getNextProbableWords(classes: List[Dict],
+                         statements: List[str]) -> Dict[str, List[str]]:
+    # Create a dictionary to store the class mappings
+    class_dict = {}
+    for class_obj in classes:
+        class_dict.update(class_obj)
+    # print(class_dict)
+
+    # Create a dictionary to store the results
+    results = {}
+
+    # Helper function to find probable words for a statement
+    def find_probable_words(statement):
+        words = statement.split('.')
+        num_dots = statement.count('.')
+        # add dots in between words
+
+        # current_class = class_dict.get(curr_word, None)
+
+        while (results.get(statement, None) is None and len(words) > 1 and num_dots > 0):
+            curr_word = words[0]
+            current_class = class_dict.get(curr_word, None)
+            if current_class is None:
+                results[statement] = [""]
+                break
+
+            if current_class is not None:
+                if isinstance(current_class, str):
+                    # Case 1: Empty class
+                    print("here str")
+                    results[statement] = [""]
+                elif isinstance(current_class, dict):
+                    # Case 2: Class containing key-value pairs
+                    print("here dict")
+                    # Case 2a: Class containing key-value pairs
+                    if len(words) == 2 and num_dots == 1:
+                        next_word = words[1]
+                        print("here dict 1")
+
+                        probable_words = [
+                            key for key in current_class.keys() if key.startswith(next_word)]
+                        results[statement] = sorted(probable_words)[:5]
+                        num_dots -= 1
+                    else:
+                        print("here dict 2")
+                        num_dots -= 1
+                        words = words[1:]
+
+                elif isinstance(current_class, list):
+
+                    # Case 3: List of strings (enum or polymorphic type)
+                    if len(words) == 2 and num_dots == 1:
+                        next_word = words[1]
+                        probable_words = [
+                            item for item in current_class if item.startswith(next_word)]
+                        results[statement] = sorted(probable_words)[:5]
+                        num_dots -= 1
+
+                    else:
+                        results[statement] = []
+
+    # Process each statement
+    for statement in statements:
+        find_probable_words(statement)
+
+    return results
+
+
+# Example usage with generic JSON data and statements
+#print(json.dumps(result, indent=2))
