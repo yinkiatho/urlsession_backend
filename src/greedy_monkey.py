f987372e31f3511a21d8dfb0eafaa2923b866fcd
yinkiatho <108441585+yinkiatho@users.noreply.github.com>
2023-09-23 17:49:54 +0800

    reverse

--- src/greedy_monkey.py
+++ src/greedy_monkey.py
@@ -23,25 +23,76 @@ input2 = {
 }
 
 
-def max_fruit_value(obj):
-    
-    w = obj["w"]
-    v = obj["v"]
-    f = obj["f"]
-    n = len(f)
-    dp = [[0] * (v + 1) for _ in range(w + 1)]
+def max_value(obj):
+    maxWeight = obj["w"]
+    volume = obj["v"]
+    fruits = obj["f"]
 
-    for i in range(n):
-        weight, volume, value = f[i]
-        for weight_limit in range(w, -1, -1):
-            for volume_limit in range(v, -1, -1):
-                if weight_limit >= weight and volume_limit >= volume:
-                    dp[weight_limit][volume_limit] = max(
-                        dp[weight_limit][volume_limit],
-                        dp[weight_limit - weight][volume_limit - volume] + value
+    basket = {}
+
+    def getTotalVolume(basket):
+        return sum(basket[key][1] for key in basket)
+
+    def getTotalWeight(basket):
+        return sum(basket[key][0] for key in basket)
+
+    for weight, vol, value in fruits:
+        if vol > volume or weight > maxWeight and not basket:
+            continue
+
+        if (getTotalVolume(basket) + vol <= volume) and (getTotalWeight(basket) + weight <= maxWeight):
+            idx = len(basket)
+            dict_key = "f." + str(idx)
+            basket[dict_key] = [weight, vol, value]
+
+        else:
+            # Sort the basket by fruit value in descending order
+            sorted_basket = sorted(
+                basket.items(), key=lambda x: x[1][2], reverse=True)
+
+            for key, (basket_weight, basket_vol, basket_value) in sorted_basket:
+                if (getTotalVolume(basket) - basket_vol + vol <= volume) and (getTotalWeight(basket) - basket_weight + weight <= maxWeight):
+                    # Calculate the new total value with the replacement
+                    new_total_value = sum(basket[k][2]
+                                          for k in basket) - basket_value + value
+
+                    if new_total_value > sum(basket[k][2] for k in basket):
+                        # Replace the fruit in the basket
+                        del basket[key]
+                        dict_key = "f." + str(len(basket))
+                        basket[dict_key] = [weight, vol, value]
+                        break
+
+    # Calculate the final total value in the basket
+    final_total_value = sum(basket[key][2] for key in basket)
+
+    return final_total_value
+
+
+def max_fruit_value(w, v, f):
+    n = len(f)
+
+    # Initialize a 3D DP table to store the maximum value
+    dp = [[[0] * (v + 1) for _ in range(w + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        weight, volume, value = f[i - 1]
+        for weight_limit in range(w + 1):
+            for volume_limit in range(v + 1):
+                # Initialize the value to the previous row's value
+                dp[i][weight_limit][volume_limit] = dp[i -
+                                                       1][weight_limit][volume_limit]
+
+                # Check if the current fruit can be included
+                if weight <= weight_limit and volume <= volume_limit:
+                    dp[i][weight_limit][volume_limit] = max(
+                        dp[i][weight_limit][volume_limit],
+                        dp[i - 1][weight_limit -
+                                  weight][volume_limit - volume] + value
                     )
 
-    return dp[w][v]
+    # The maximum value is in the last cell of the DP table
+    return dp[n][w][v]
 
 def maxValue(obj):
     #print(obj)
@@ -88,7 +139,7 @@ def maxValue(obj):
                 
     return sum([basket[key][2] for key in basket])
                 
-#print(maxValue(input))  
+#print(max_fruit_value(input["w"], input["v"], input["f"]))  
 if __name__ == "__main__":
     # Check if the script is being run directly
     if len(sys.argv) != 2:
@@ -100,7 +151,7 @@ if __name__ == "__main__":
     #print(json_data)
     try:
         data = json.loads(json_data)
-        result = maxValue(data)
+        result = max_fruit_value(data)
         print(result)  # Print the result to stdout
     except Exception as e:
         print(f"Error: {str(e)}")
